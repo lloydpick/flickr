d2fc6162682cd1c004429ca08bcff72c77fa7460
Sebastian Johnsson <sebastian@Sebastian-Johnssons-MacBook.local>
2010-09-04 15:09:17 +0200

    Added Original size to sizes constant

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -59,10 +59,11 @@ module Flickr
   # but its source is at 
   # "http://farm4.static.flickr.com/3118/2397458775_2ec2ddc324_m.jpg". 
   # The VALID_SIZES hash associates the correct letter with a label
-  VALID_SIZES = { "Square" => ["s", "sq"],
+  VALID_SIZES = { "Square"    => ["s", "sq"],
                   "Thumbnail" => ["t", "t"],
-                  "Small" => ["m", "s"],
-                  "Medium" => [nil, "m"],
-                  "Large" => ["b", "l"]
+                  "Small"     => ["m", "s"],
+                  "Medium"    => [nil, "m"],
+                  "Large"     => ["b", "l"],
+                  "Original"  => ["o", "o"]
                 }
 end
